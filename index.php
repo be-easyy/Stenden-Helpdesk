30a5c78748196d442b2beb7406c2911314df1e8a
TomiEckert <tamas.eckert@student.nhlstenden.com>
2018-12-13 11:51:56 +0100

    added basic includes

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    include("include/init-db.php");
+    include("include/init-session.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Stenden Helpdesk</title>
+</head>
+<body>
+    
+</body>
+</html>
