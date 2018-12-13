0ca1375512499cd179e63bc41504aad3c6b9004b
TomiEckert <tamas.eckert@student.nhlstenden.com>
2018-12-13 15:31:28 +0100

    Merged Skrrrr's page with the php

--- index.php
+++ index.php
@@ -6,11 +6,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Stenden Helpdesk</title>
+    <link rel="stylesheet" href="style/style.css" type="text/css">
+    <title>Login - Stenden Helpdesk</title>
 </head>
 <body>
-    
+    <div class="page">
+		<div class="mid_box">
+			<div class="mid_box_margin">
+				<div class="mid_box_margin_logo">
+					<img src="img/" alt="Logo">
+				</div>
+				<div class="mid_box_margin_form">
+					<form action="" method="POST">
+						<input type="text" id="input" name="user">
+						<input type="password" id="input" name="pass">
+						<input type="submit" id="submit" name="submit">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
