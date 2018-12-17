4a8049a1dda2ceb62ed817f8b544c78ae6a7d658
TomiEckert <tamas.eckert@student.nhlstenden.com>
2018-12-17 12:41:09 +0100

    re-added "init" and "db" scripts

--- includes/init-db.php
+++ includes/init-db.php
@@ -1,3 +1,82 @@
 <?php
-    
+    $Host = "localHost";
+    $User = "root";
+    $Pass = "rabbit"; // TODO change me if necessary
+    $Database = "SupportDesk";
+    $SQLConnect = mysqli_connect($Host, $User, $Pass);
+    if(!$SQLConnect) {
+        echo mysqli_error($SQLConnect);
+        return;
+    }
+    $DBBool = mysqli_select_db($SQLConnect, $Database);
+    if(!$DBBool) {
+        echo mysqli_error($SQLConnect);
+        $SQLQuery = "CREATE DATABASE SupportDesk";
+        $stmt = mysqli_prepare($SQLConnect, $SQLQuery);
+        mysqli_execute($stmt);
+    }
+    $DBBool = mysqli_select_db($SQLConnect, $Database);
+    if(!$DBBool) {
+        echo mysql_error($SQLConnect);
+        return;
+    }
+    $SQLQuery = "DESCRIBE Incident";
+    $result = mysqli_query($SQLConnect, "select * from Type");
+    if(!$result) {
+        echo "<h1>PLEASE REFRESH THE PAGE</h1><hr>database initialized";
+        $queries = array(
+            "CREATE TABLE `Incident` (
+                `Incident_ID` INT NOT NULL,
+                `Time_Registered` TIME NOT NULL,
+                `Client_ID` INT NOT NULL,
+                `Date` DATE NOT NULL,
+                `Description` varchar(280) NOT NULL,
+                `Type_ID` INT NOT NULL,
+                `Other` varchar(30),
+                `Solution_ID` INT NOT NULL,
+                `Employee_ID` INT NOT NULL,
+                PRIMARY KEY (`Incident_ID`,`Time_Registered`)
+            );",
+            "CREATE TABLE `Type` (
+                `Type_ID` INT NOT NULL AUTO_INCREMENT,
+                `Type_Name` varchar(30) NOT NULL,
+                PRIMARY KEY (`Type_ID`)
+            );",
+            "CREATE TABLE `Employees` (
+                `Employee_ID` INT NOT NULL AUTO_INCREMENT,
+                `Employee_Name` varchar(30) NOT NULL,
+                `Employee_Image` varchar(30) NOT NULL,
+                `Employee_Pass` varchar(40) NOT NULL,
+                `Employee_Permission` INT NOT NULL,
+                PRIMARY KEY (`Employee_ID`)
+            );",
+            "CREATE TABLE `Solution` (
+                `Solution_ID` INT NOT NULL AUTO_INCREMENT,
+                `Solution_Description` varchar(300) NOT NULL,
+                PRIMARY KEY (`Solution_ID`)
+            );",
+            "CREATE TABLE `Client` (
+                `Client_ID` INT NOT NULL AUTO_INCREMENT,
+                `Client_Name` varchar(30) NOT NULL,
+                `Client_Pass` varchar(40) NOT NULL,
+                `Client_License` INT NOT NULL,
+                PRIMARY KEY (`Client_ID`)
+            );",
+            "ALTER TABLE `Incident` ADD CONSTRAINT `Incident_fk0` FOREIGN KEY (`Client_ID`) REFERENCES `Client`(`Client_ID`);",
+            "ALTER TABLE `Incident` ADD CONSTRAINT `Incident_fk1` FOREIGN KEY (`Type_ID`) REFERENCES `Type`(`Type_ID`);",
+            "ALTER TABLE `Incident` ADD CONSTRAINT `Incident_fk2` FOREIGN KEY (`Solution_ID`) REFERENCES `Solution`(`Solution_ID`);",
+            "ALTER TABLE `Incident` ADD CONSTRAINT `Incident_fk3` FOREIGN KEY (`Employee_ID`) REFERENCES `Employees`(`Employee_ID`);"
+        );
+        
+        foreach ($queries as $SQLQuery) {
+            $stmt = mysqli_prepare($SQLConnect, $SQLQuery);
+            if($stmt) {
+                mysqli_execute($stmt);
+            } else {
+                echo mysqli_error($SQLConnect);
+                return;
+            }
+        }
+    }
+    mysqli_close($SQLConnect);
 ?>
