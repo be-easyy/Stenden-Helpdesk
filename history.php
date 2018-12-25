784748d5d7739143099526c61e2459389b73de7f
Aleksei Skorjak <43403562+AlekseiSkr@users.noreply.github.com>
2018-12-25 07:49:21 +0100

    New logo and some more css
    b o i

---
+++ history.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/style.css" type="text/css">
+    <title>User - Stenden Helpdesk</title>
+</head>
+<body>
+<div class="page">
+    <div class="wrap">
+        <div class="header">
+            <div class="logo">
+            <img id="logo" src="img/logo.png" alt="Logo">
+            </div>
+            <div class="navbar">
+                <a class="open" href="#">New Ticket</a> 
+                <a href="#">Ticket History</a> 
+                <a href="#">Overview</a> 
+                <a href="#">Your Tickets</a>
+                <a href="">FAQ</a>
+            </div>
+        </div>
+
+
+        <div class="content">
+            <div class="content_margin">
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="footer_margin">
+        </div>
+    </div>
+</div>
+</body>
+</html>
