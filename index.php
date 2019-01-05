9f64262090e6fe94d99486411f70bfe40e4033b0
sytzewest <43606602+sytzewest@users.noreply.github.com>
2019-01-05 12:33:16 +0100

    Start to the code, not 100% working yet

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    include("include/init-db.php");
-    include("include/init-session.php");
+    include("includes/init-db.php");
+    include("includes/init-session.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,8 +17,14 @@
 					<img id="logo" src="img/logo.png" alt="Logo">
 				</div>
 				<div class="mid_box_margin_form">
-					<form action="" method="POST">
+					<form action="log_in.php" method="POST">
+						<div class="login_input">
+							Username: 
+						</div>
 						<input type="text" id="input" name="user">
+						<div class="login_input">
+							Password: 
+						</div>
 						<input type="password" id="input" name="pass">
 						<input type="submit" id="submit" name="submit">
 					</form>
