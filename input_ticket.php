7b7de11953fb41265381dfef4375181ac943b1d9
sytzewest <43606602+sytzewest@users.noreply.github.com>
2019-01-15 18:57:25 +0100

    Update input_ticket.php

--- input_ticket.php
+++ input_ticket.php
@@ -1,8 +1,8 @@
 <?php
-include("./includes/init-db.php");
-include("./includes/init-session.php");
-include("./includes/check-login.php");
-CheckClient();
+    include("./includes/init-db.php");
+    include("./includes/init-session.php");
+    include("./includes/check-login.php");
+    CheckClient();
 ?>
 
 <!DOCTYPE html>
@@ -21,73 +21,27 @@ CheckClient();
             <img id="logo" src="img/logo.png" alt="Logo">
             </div>
             <div class="navbar">
-                <a class="open" href="./input_ticket.php">New Ticket</a> 
-                <a href="./history.php">Ticket History</a> 
-                <a href="./overview_client.php">Overview</a> 
-                <a href="./client_tickets.php">Your Tickets</a>
-                <a href="./faq.html">FAQ</a>
+                <a class="open" href="input_ticket.php">New Ticket</a> 
+                <a href="history.php">Ticket History</a> 
+                <a href="overview_client.php">Overview</a> 
+                <a href="#">Your Tickets</a>
+                <a href="faq.html">FAQ</a>
             </div>
         </div>
         <div class="content">
             <div class="content_margin">
             <h2>Ticket Submission - Stenden Helpdesk</h2>
-
-            
-<?php
-if (isset($_POST['submit'])) {
-
-    if (empty($_POST['desc']) || empty($_POST['type'])) {
-        echo "<p>You must fill in all the required elements.
-            Click your browser's back button to return to the message form.</p>";
-    } else {
-        $SQLConnect = OpenDBConnection();
-        
-        $desc = htmlentities($_POST['desc']);
-        $type = htmlentities($_POST['type']);
-
-        $fields = array('Time_Registered', 'Client_ID', 'Date', 'Description', 'Type_ID', 'Status');
-        $values = array('CURRENT_TIME', '1', 'CURRENT_DATE', $desc, $type, '0'); // TODO change NULL to CLient_ID
-
-        $stmt = InsertDBStatement($SQLConnect, "Incident", $fields, $values, "issi");
-        if ($stmt != false) {
-            $QueryResult2 = $stmt->execute();
-            if ($QueryResult2 === false) {
-                DisplayDBError($SQLConnect);
-            } else {
-                echo "<h1>Thank you for submitting your ticket!</h1>";
-            }
-            $stmt->close();
-            CloseDBConnection($SQLConnect);
-        } else {
-            echo "error";
-        }
-    }
-}
-?>
-
-
         <form method="POST" action="input_ticket.php">
-            <p>
-                <label for="desc">Description</label>
-            </p>
-            <p>
-                <textarea name="desc" id="desc" placeholder="Description..."></textarea>
-            </p>
-            <p>
-                <label for="dropdown">Type of Issue</label>
-            </p>
-            <p>
-                <select name="type" id="dropdown">
-                    <option value="1">Technical Problem</option>
-                    <option value="2">Functional Problem</option>
-                    <option value="3">Failure</option>
-                    <option value="4">Question</option>
-                    <option value="5">Wish</option>
-                </select>
-            </p>
-            <p>
-                <input type="submit" name="submit" value="Submit">
-            </p>
+            <p>Description <input type="text" name="desc"/></p>
+            <p>Type of Issue  
+            <select name="issue">
+            <option value="tech">Technical Problem</option>
+            <option value="func">Functional Problem</option>
+            <option value="fail">Failure</option>
+            <option value="question">Question</option>
+            <option value="wish">Wish</option>
+            </select></p>
+            <p><input type="submit" value="Submit"></p>
         </form>
          </div>
         </div>
@@ -100,3 +54,42 @@ if (isset($_POST['submit'])) {
 </div>
     </body>
 </html>
+
+<?php
+    if(!isset($_POST['submit'])){
+    
+    if (empty($_POST['desc']) || empty($_POST['issue'])) {
+        echo "<p>You must fill in all the required elements.
+            Click your browser's back button to return to the message form.</p>";
+    } else {
+        include ("includes/init-db.php");
+
+        mysqli_select_db($DBConnect, $db_name);
+
+        $desc = htmlentities($_POST['desc']);
+        $type = htmlentities($_POST['issue']);
+
+        $SQLstring2 = "";
+
+        if ($stmt = mysqli_prepare($DBConnect, $SQLstring2)) {
+            mysqli_stmt_bind_param($stmt, 'ss', $desc, $type);
+            $QueryResult2 = mysqli_stmt_execute($stmt);
+            if ($QueryResult2 === FALSE) {
+                echo "<p>Unable to execute the query.</p>"
+                . "<p>Error code "
+                . mysqli_errno($DBConnect)
+                . ": "
+                . mysqli_error($DBConnect)
+                . "</p>";
+            } else {
+                echo "<h1>Thank you for submitting your ticket!</h1>";
+            }
+            mysqli_stmt_close($stmt);
+        } else {
+            echo "error";
+        }
+        mysqli_close($DBConnect);
+    }
+}
+?>
+
