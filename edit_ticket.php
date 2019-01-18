2e69de0f9d5eeeca0cd2032fa3b9d4dda68e9474
Glusis <philip.blesinger@student.nhlstenden.com>
2019-01-18 08:27:44 +0100

    edit_ticket but idk

--- edit_ticket.php
+++ edit_ticket.php
@@ -3,6 +3,7 @@
     include("./includes/init-session.php");
     include("./includes/check-login.php");
     CheckEmployee();
+    $error = NULL;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,7 +33,40 @@
         </div>
         <div class="content">
             <div class="content_margin">
-            
+                <form method='POST' action='<?php htmlentities($_SERVER['PHP_SELF'])?>'>
+                    <textarea name='sol'>
+                        <?php
+                            if($db = new PDO('mysql:host=localhost;dbname=supportdesk;charset=utf8', 'root', '')){
+                                if($stmt=$db->prepare('SELECT solution_description FROM solution WHERE solution_ID = ?')){
+                                    if($stmt->execute(array(htmlentities($_GET['id'])))){
+                                        $solution = $stmt->fetchAll();
+                                        //var_dump($solution);
+                                        foreach($solution as $sth){
+                                            echo $solution[0][0];
+                                        }
+                                    } else {echo'could not execute';}
+                                }else{echo'could not prepare';}
+                            }//else{echo'could not connect to database';}
+                        ?>
+                    </textarea>
+                    <input type="submit" name="submit">
+                </form>
+                <?php
+                    if (isset($_POST['submit'])){
+                        //if(!empty($_POST['sol'])){
+                            if($db = new PDO('mysql:host=localhost;dbname=supportdesk;charset=utf8', 'root', '')){
+                                if($stmt=$db->prepare('UPDATE solution, SET solution_description = ? WHERE solution_ID = ?')){
+                                    if($stmt->execute(array(htmlentities($_POST['sol']), htmlentities($_GET['id'])))){
+                                        echo 'successfully updated ticket';
+                                        $stmt = NULL;
+                                        $db = NULL;
+                                    }$error = 'could not execute';
+                                }$error = 'could not prepare';
+                            }$error = 'could not connect to database';
+                        //}$error = 'please fill in a solution';
+                    }
+                    echo $error;
+                ?>
             </div>
         </div>
     </div>
